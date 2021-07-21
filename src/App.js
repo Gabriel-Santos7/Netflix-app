561375e5bb3622f5321998b09d684cc531a8829a
Gabriel-Santos7 <gabrielborgessantos721@gmail.com>
2021-07-21 19:07:33 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line
+import React, {useEffect, useState} from 'react';
+// eslint-disable-next-line
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
+// eslint-disable-next-line
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista dos filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      //Pegando o filme em destaque
+      let originals = list.filter(i=> i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+    }
+
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+      
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
